a248a90d23ba587e5e8ffe82776a430625ebbba9
AndrewZinke <azinke96@gmail.com>
2020-08-26 11:33:20 -0500

    Initial Upload

---
+++ Controllers/newFAQController.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace MVCWithOath.Controllers
+{
+    public class newFAQController
+    {
+        
+    }
+}
---
+++ Models/newFAQModel.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace MVCWithOath.Models
+{
+    public class newFAQModel
+    {
+        public string newQuestion { get; set; }
+    }
+}
